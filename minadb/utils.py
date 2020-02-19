016caa46e678e1bbce1414ee254a1f14609cd537
williamfzc <178894043@qq.com>
2020-02-19 17:16:17 +0800

    feat: move stderr to stdout

--- minadb/utils.py
+++ minadb/utils.py
@@ -3,7 +3,7 @@ import typing
 
 
 def run_cmd(cmd: typing.List[str], no_decode: bool = None) -> typing.Union[bytes, str]:
-    r = subprocess.check_output(cmd)
+    r = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
     if no_decode:
         return r
     return r.decode()
