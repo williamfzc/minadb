0d60869cd134e4fc98a08d8dad3ff466c6495b86
williamfzc <178894043@qq.com>
2020-02-20 16:06:35 +0800

    feat: process manager

--- minadb/device.py
+++ minadb/device.py
@@ -1,6 +1,10 @@
 import subprocess
 import typing
 import time
+try:
+    from loguru import logger as logging
+except ImportError:
+    import logging
 
 from minadb.utils import run_cmd
 
@@ -31,7 +35,50 @@ class _BaseADBDevice(object):
         return run_cmd(self.build_no_shell_cmd(cmd))
 
 
+class _Process(object):
+    def __init__(
+        self,
+        user: str,
+        pid: int,
+        ppid: int,
+        vsz: int,
+        rss: int,
+        wchan: str,
+        addr: str,
+        s: str,
+        name: str,
+        *unknown,
+    ):
+        self.user = user
+        self.pid = pid
+        self.ppid = ppid
+        self.vsz = vsz
+        self.rss = rss
+        self.wchan = wchan
+        self.addr = addr
+        self.s = s
+        self.name = "".join([name, *unknown])
+
+
 class ADBDevice(_BaseADBDevice):
+    def ps(self) -> typing.List[_Process]:
+        raw: str = self.shell(["ps"])
+        proc_list: typing.List[str] = raw.split("\n")[1:-1]
+        proc_list: typing.List[typing.List[str]] = [
+            [i for i in each.split(" ") if i] for each in proc_list
+        ]
+        proc_list: typing.List[_Process] = [_Process(*each) for each in proc_list]
+        return proc_list
+
+    def kill_process_by_id(self, process_id: int, signal: int = -9) -> str:
+        return self.shell(["kill", str(signal), process_id])
+
+    def kill_process_by_name(self, process_name: str, signal: int = -9):
+        for each in self.ps():
+            if process_name in each.name:
+                return self.kill_process_by_id(each.pid, signal)
+        logging.error(f"no process named: {process_name}")
+
     def screen_record(self) -> typing.Callable:
         device_path = f"/data/local/tmp/{int(time.time())}.mp4"
         full_cmd = self.build_shell_cmd(["screenrecord", device_path])
@@ -42,9 +89,7 @@ class ADBDevice(_BaseADBDevice):
         def stop(pc_path: str = None) -> str:
             proc.terminate()
             proc.kill()
-
-            # todo at least system6
-            self.shell(["pkill", "screenrecord"])
+            self.kill_process_by_name("screenrecord")
 
             return self.pull(device_path, pc_path)
 
--- minadb/utils.py
+++ minadb/utils.py
@@ -1,8 +1,10 @@
 import subprocess
 import typing
+from minadb.device import logging
 
 
 def run_cmd(cmd: typing.List[str], no_decode: bool = None) -> typing.Union[bytes, str]:
+    logging.info(f"run command: {cmd}")
     r = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
     if no_decode:
         return r
