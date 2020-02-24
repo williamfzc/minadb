5dfb49737430734eb46f60125eddd3edf96875ee
williamfzc <178894043@qq.com>
2020-02-24 12:31:37 +0800

    feat: more api for client

--- minadb/client.py
+++ minadb/client.py
@@ -10,3 +10,18 @@ class ADBClient(object):
 
         # [['123456E', 'device'], ['123456F', 'offline']]
         return raw_device_list
+
+    def available_devices(self) -> typing.List[typing.List[str]]:
+        device_list = self.devices()
+        return [each for each in device_list if each[1] == "device"]
+
+    def is_device_available(self, serial_no: str) -> bool:
+        return serial_no in [each[0] for each in self.available_devices()]
+
+
+if __name__ == "__main__":
+    cli = ADBClient()
+    d = cli.available_devices()
+    print(d)
+    a = cli.is_device_available("123456F")
+    print(a)
