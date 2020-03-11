a7eb385342e2b8ddd4debc4ae832b1c70aff412a
williamfzc <178894043@qq.com>
2020-03-11 20:11:24 +0800

    feat(cli): make it look like adb

--- minadb/cli.py
+++ minadb/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import pprint
 
 from minadb import ADBDevice
 
@@ -6,12 +7,13 @@ from minadb import ADBDevice
 def main():
     parser = argparse.ArgumentParser(description="minadb cli")
     parser.add_argument("-s", "--serial_no", nargs="?")
-    parser.add_argument("-c", "--command", nargs="*")
+    parser.add_argument("command", nargs="*", default="")
     args = parser.parse_args()
 
     serial_no = args.serial_no
     command = args.command
     if not command:
+        pprint.pprint(ADBDevice.__dict__)
         raise RuntimeError("no command")
     func = command[0]
     extras = command[1:]
